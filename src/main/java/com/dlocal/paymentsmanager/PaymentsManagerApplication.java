791163e194eb4aa6d4e5e1ab81bef7bf4c6081de
Rodrigo Leira <rodrigoleira@gmail.com>
2018-11-11 15:41:02 -0300

    Initial commit

---
+++ src/main/java/com/dlocal/paymentsmanager/PaymentsManagerApplication.java
@@ -0,0 +1,12 @@
+package com.dlocal.paymentsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsManagerApplication.class, args);
+	}
+}
