441dd49ce41622bce8e772f51e4f3b99964d6387
Rodrigo Leira <rodrigoleira@gmail.com>
2018-11-11 15:42:48 -0300

    Configure Jetty

--- src/main/java/com/dlocal/paymentsmanager/PaymentsManagerApplication.java
+++ src/main/java/com/dlocal/paymentsmanager/PaymentsManagerApplication.java
@@ -1,12 +1,34 @@
 package com.dlocal.paymentsmanager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
-public class PaymentsManagerApplication {
+public class PaymentsManagerApplication extends SpringBootServletInitializer {
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory()
+	{
+		JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+		factory.setContextPath("/api");
+		factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
+		return factory;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentsManagerApplication.class, args);
+		new PaymentsManagerApplication().configure(new SpringApplicationBuilder(PaymentsManagerApplication.class)).run(args);
 	}
 }
