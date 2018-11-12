e37d912a49a9cbd41731d23d9fabf0ea23a75687
Rodrigo Leira <rodrigoleira@gmail.com>
2018-11-11 22:53:45 -0300

    Adding more filters to payments repository

--- src/main/java/com/dlocal/paymentsmanager/datastore/dal/PaymentRepository.java
+++ src/main/java/com/dlocal/paymentsmanager/datastore/dal/PaymentRepository.java
@@ -1,5 +1,6 @@
 package com.dlocal.paymentsmanager.datastore.dal;
 
+import com.dlocal.paymentsmanager.datastore.enums.PaymentCurrency;
 import com.dlocal.paymentsmanager.datastore.enums.PaymentStatus;
 import com.dlocal.paymentsmanager.datastore.models.Payment;
 import org.springframework.data.mongodb.repository.Query;
@@ -11,4 +12,16 @@ public interface PaymentRepository extends PagingAndSortingRepository<Payment, S
 
     @Query
     public Iterable<Payment> findPaymentsByPaymentStatus(PaymentStatus paymentStatus);
+
+    public Iterable<Payment> findPaymentsByMerchantIdOrderByAmountAsc(String merchantId);
+    public Iterable<Payment> findPaymentsByMerchantIdOrderByAmountDesc(String merchantId);
+
+    public Iterable<Payment> findPaymentsByTransactionIdOrderByAmountAsc(String transactionId);
+    public Iterable<Payment> findPaymentsByTransactionIdOrderByAmountDesc(String transactionId);
+
+    public Iterable<Payment> findPaymentsByAmountOrderByAmountAsc(Double amount);
+    public Iterable<Payment> findPaymentsByAmountOrderByAmountDesc(Double amount);
+
+    public Iterable<Payment> findPaymentsByCurrencyOrderByAmountAsc(PaymentCurrency currency);
+    public Iterable<Payment> findPaymentsByCurrencyOrderByAmountDesc(PaymentCurrency currency);
 }
